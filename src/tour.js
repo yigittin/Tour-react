031d4626e4ae20afd3124037fd16133e45673b72
yigittin <i.yigittin@hotmail.com>
2021-05-21 22:42:31 +0300

    read more added

--- src/tour.js
+++ src/tour.js
@@ -1,29 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import {data} from './data'
-const url=data.image;
 const Tour = () => {    
     const [isMore, setIsMore]=useState(false);
-    const[tours,setTours]=useState(data);
-    useEffect(()=>{
-       
-  },[]);
-    return(<>
-           {tours.map((tour=>{   
-                const{id,name,image,text,price}=tour;  
-                    return(   
-                        <div className='tourdiv'>                            
-                            <article key={id} className='tourart'>  
-                                <img className='img' src={image} alt="" />                  
-                                <h3 className='turlar'>{name}<h5 className='price'>${price}</h5></h3>                         
-                                <p>{text}<button className='read'>Read more</button></p> 
-                            <button >delete</button>                     
-                        </article>
-                        </div>)                 
-             }))}
-             
-        </>)
+    const[tours,setTours]=useState(data);   
+    return(<>           
+        <div className='tourdiv'>                            
+            <Article/> 
+        </div>               
+    </>)
         
     
+}
+
+const Article=()=>{
+    const[tours,setTours]=useState(data);  
+    const [isLoading, setIsLoading]=useState(true); 
+    const [show,setShow]=useState(false);
+    useEffect(()=>{
+        setIsLoading(true);
+        tours.map((tour=>{   
+                    const{id,name,image,text,price}=tour;  
+                    fetch(image).then((resp)=>{
+                        if(resp.status>=200&&resp.status<=299){                         
+                            setIsLoading(false);}                
+            })            
+        }))},[])              
+            
+    
+     if(isLoading){
+         return(
+       <div>
+      <h2>Loading...</h2>
+      </div>);         
+     }   
+    
+    return (
+        tours.map((tour=>{   
+                const{id,name,image,text,price}=tour;
+        return(     
+        < >
+            <article key={id} className='tourart'>  
+                <img className='img' src={image} alt="" />                  
+                <h3 className='turlar'>{name}<h5 className='price'>${price}</h5></h3>                                       
+                {show &&  <section>
+                 <p key={id} className='info'>{text}</p><button className='read'
+                 onClick={()=>setShow(!show)}>Read more</button>                          
+                
+                </section> } 
+                {!show&& <section>
+                    <p key={id} >{text}<button className='read'
+                    onClick={()=>setShow(!show)}>Show Less</button>   </p>                
+                </section> }   
+                <button >delete</button>               
+            </article>
+        </>);
+        }))  
+        
+    )
 }
 
 export default Tour
